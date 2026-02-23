64e721059cdaa08fa4e325790cb65279c5532110
Carlos <carberman2@alu.edu.gva.es>
2026-02-24 00:29:26 +0100

    Documentacion swagger

--- src/main/java/com/carlos/hogwarts/controller/AsignaturaController.java
+++ src/main/java/com/carlos/hogwarts/controller/AsignaturaController.java
@@ -1,13 +1,16 @@
 package com.carlos.hogwarts.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.carlos.hogwarts.service.AsignaturaService;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlos.hogwarts.service.AsignaturaService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 
@@ -17,8 +20,14 @@ import lombok.RequiredArgsConstructor;
 public class AsignaturaController {
     private final AsignaturaService asignaturaService;
 
+    @Operation(summary = "Elimina un estudiante dada una ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Asignatura eliminada con éxito"),
+        @ApiResponse(responseCode = "404", description = "Asignatura no encontrada"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarEstudiante(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminarAsignatura(@PathVariable Long id) {
         asignaturaService.eliminarAsignatura(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/carlos/hogwarts/controller/EstudianteController.java
+++ src/main/java/com/carlos/hogwarts/controller/EstudianteController.java
@@ -20,6 +20,9 @@ import com.carlos.hogwarts.dtos.request.update.EstudianteUpdateDTO;
 import com.carlos.hogwarts.dtos.response.EstudianteDTO;
 import com.carlos.hogwarts.service.EstudianteService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -30,6 +33,8 @@ import lombok.RequiredArgsConstructor;
 public class EstudianteController {
     private final EstudianteService estudianteService;
 
+    @Operation(summary = "Endpoint de prueba")
+    @ApiResponse(responseCode = "200", description = "El servicio está activo y respondiendo")
     @GetMapping("/test")
     public Map<String, String> probarEndpoint() {
         Map<String, String> respuesta = new HashMap<>();
@@ -38,6 +43,11 @@ public class EstudianteController {
         return respuesta;
     }
 
+    @Operation(summary = "Obtiene todos los estudiantes")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Lista de estudiantes obtenida con éxito"),
+        @ApiResponse(responseCode = "204", description = "No hay estudiantes registrados en la base de datos")
+    })
     @GetMapping
     public ResponseEntity<List<EstudianteDTO>> obtenerTodos() {
         List<EstudianteDTO> estudiantes = estudianteService.obtenerTodos();
@@ -48,18 +58,34 @@ public class EstudianteController {
         return ResponseEntity.ok(estudiantes);
     }
 
+    @Operation(summary = "Obtiene un estudiante dado un ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Estudiante encontrado"),
+        @ApiResponse(responseCode = "404", description = "Estudiante no encontrado")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<EstudianteDTO> obtenerEstudiantePorId(@PathVariable Long id) {
         EstudianteDTO dto = estudianteService.obtenerEstudiantePorId(id);
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Crea un estudiante")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Estudiante creado exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos (Error de validación)")
+    })
     @PostMapping
     public ResponseEntity<EstudianteDTO> crearEstudiante(@Valid @RequestBody EstudianteCreateDTO dto) {
         EstudianteDTO estudianteCreado = estudianteService.crearEstudiante(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(estudianteCreado);
     }
 
+    @Operation(summary = "Modifica un estudiante")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Estudiante actualizado correctamente"),
+        @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos"),
+        @ApiResponse(responseCode = "404", description = "No se encontró el estudiante con el ID proporcionado")
+    })
     @PutMapping("/{id}")
     // Parece ser que por motivos de seguridad, en versiones recientes de Spring,
     // hay que especificarle en @PathVariable el nombre de la variable
@@ -68,6 +94,12 @@ public class EstudianteController {
         return ResponseEntity.ok(estudianteActualizado);
     }
 
+    @Operation(summary = "Elimina un estudiante")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Estudiante eliminado con éxito"),
+        @ApiResponse(responseCode = "404", description = "Estudiante no encontrado"),
+        @ApiResponse(responseCode = "500", description = "Error interno al intentar eliminar")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarEstudiante(@PathVariable Long id) {
         estudianteService.eliminarEstudiante(id);
